583ddee5e9d23fc032d50f73c97fd4a4e5ecc066
ElisaBorba <elisaborbaa@gmail.com>
2023-12-08 11:19:47 -0300

    migration de users

---
+++ src/migrations/20231208135632-users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    }, {
+      underscored: true,
+      tableName: 'users'
+    });
+
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
