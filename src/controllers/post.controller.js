d0b15e2067c9ac63e613e58c7688c621a41672ff
ElisaBorba <elisaborbaa@gmail.com>
2024-03-26 11:56:20 -0300

    finish

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -3,14 +3,13 @@ const { postService } = require('../services');
 const addPost = async (req, res) => {
   try {
     const { title, content, categoryIds } = req.body;
-
     const { id: userId } = req.user;
-
-    console.log('USEEEER', userId);
-    
-    const newPost = await postService.addPost({ title, content, userId, categoryIds });
-
-    console.log('POOOOOOST', newPost);
+    const newPost = await postService.addPost({
+      title,
+      content,
+      userId,
+      categoryIds,
+    });
 
     res.status(201).json(newPost);
   } catch (error) {
